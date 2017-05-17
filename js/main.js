4cdd92fec9dda20a8872ad9508ee2e946d64b73f
RangiBecerra <rangibecerra@gmail.com>
2017-05-17 17:41:02 -0400

    boton, lista

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 //alert("existe");
+function principal() {
+	var lista = document.getElementById("agregar").value;
+	var contenedor = document.getElementById("listado");
+	var res = document.createElement("p");
+    var texto = document.createTextNode("lista");
+    contenedor.appendChild(res);
+    res.appendChild(texto);
+    console.log(lista);
+}
